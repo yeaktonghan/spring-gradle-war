81f8d20f1c4be84f344628b20de7b9528745490c
tong <tong@pop-os.localdomain>
2023-11-24 15:45:48 +0700

    first commit

---
+++ src/main/java/com/example/demo/GradleWarApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleWarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleWarApplication.class, args);
+	}
+
+}
